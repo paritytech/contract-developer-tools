4ec33c10efcc546c875aeb6f96054e6de047d934
Charles Hetterich <hetterich.charles@gmail.com>
2026-01-22 21:49:20 +0000

    adding [product-preview-net](https://github.com/paritytech/product-preview-net) dependencies

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,6 @@
+# Fetch product preview network dependencies
+curl -sSL https://raw.githubusercontent.com/paritytech/ppn-proxy/main/install.sh | bash
+
 cd examples/market-api
 
 mkdir -p .papi/descriptors
